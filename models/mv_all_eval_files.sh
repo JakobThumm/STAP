25ca758b40bbdec802b9071f42090db8e58ff2e0
JakobThumm <jakob.thumm@tum.de>
2024-08-23 12:04:57 -0700

    Added a script to move all eval files to the correct folder.

---
+++ models/mv_all_eval_files.sh
@@ -0,0 +1,7 @@
+mv eval/planning/object_arrangement_additive eval/planning/object_arrangement/additive_baseline
+mkdir eval/planning/object_arrangement/generated_ablation/
+mv eval/planning/object_arrangement/generated_ablation_* eval/planning/object_arrangement/generated_ablation/
+mkdir eval/planning/object_arrangement/baseline/
+mv eval/planning/object_arrangement/ablation_* eval/planning/object_arrangement/baseline/
+mkdir eval/planning/object_arrangement/oracle/
+mv eval/planning/object_arrangement/oracle_* eval/planning/object_arrangement/oracle/
