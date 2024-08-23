07c0b4bfc6592b13b06c6225f3cf159cf2e4733e
JakobThumm <jakob.thumm@tum.de>
2024-08-23 12:37:36 -0700

    Added all model checkpoints

--- scripts/download/download_checkpoints.sh
+++ scripts/download/download_checkpoints.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Datasets
-
+wget -p models/ https://nextcloud.in.tum.de/index.php/s/twGzepiaqoq8kqm/download/datasets_initial_submission.zip
+unzip models/datasets_initial_submission.zip
 
 # Q-value functions
-wget https://nextcloud.in.tum.de/index.php/s/8RyNzX79aBrjWzT/download/value_fns_initial_submission.zip
-unzip value_fns_initial_submission.zip
+wget -p models/ https://nextcloud.in.tum.de/index.php/s/8RyNzX79aBrjWzT/download/value_fns_initial_submission.zip
+unzip models/value_fns_initial_submission.zip
 
 # Policies
-wget https://nextcloud.in.tum.de/index.php/s/xXtATYPWnyG5awE/download/policy_irl_initial_submission.zip
-unzip policy_irl_initial_submission.zip
+wget -p models/ https://nextcloud.in.tum.de/index.php/s/xXtATYPWnyG5awE/download/policy_irl_initial_submission.zip
+unzip models/policy_irl_initial_submission.zip
 
 # Dynamics
-wget https://nextcloud.in.tum.de/index.php/s/gqQbd4KPwK9ogjR/download/dynamics_irl_initial_submission.zip
-unzip dynamics_irl_initial_submission.zip
+wget -p models/ https://nextcloud.in.tum.de/index.php/s/gqQbd4KPwK9ogjR/download/dynamics_irl_initial_submission.zip
+unzip models/dynamics_irl_initial_submission.zip
